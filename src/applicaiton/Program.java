214bff41f55747f4a9a42cac419769f69831cdef
João Gabriel <jgmoreira15@gmail.com>
2019-08-26 22:16:08 -0300

    Recuperação de Dados

--- src/applicaiton/Program.java
+++ src/applicaiton/Program.java
@@ -1,16 +1,49 @@
 package applicaiton;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
+import db.DbException;
 
 public class Program {
 
 	public static void main(String[] args) {
+		//Conecta ao banco
+		Connection conn = null;
+		//Prepara uma consulta SQL
+		Statement st = null;
+		//Resultado da consulta é armazedo em rs
+		ResultSet rs = null;
 		
-		Connection con = DB.getConnection();
 		
-		DB.closeConnetcion();
+		try {
+			//O método foi chamado direto porque é um método estático, logo não precisa ser instanciado. Chamada a classe + método;
+			conn = DB.getConnection();
+			//Para instanciar um objeto do tipo statement, chama a variável de conexão que já está instanciada e o método createStatement;
+			st = conn.createStatement();
+			//Seleciona a tabela "departamento" do BD, o resultado é atribuido na variável ResultSet
+			rs = st.executeQuery("select * from department");
+			
+			//Enquanto existir um próximo, faça;
+			while(rs.next()) {
+				//Pega um número inteiro que está na tabela "Id" e pega uma String que está na tabela Name"
+				System.out.println(rs.getInt("Id")+ ", " + rs.getString("Name"));
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		
+		finally {
+			//Fecha as conexões para não haver vazamento de memória
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);;
+			DB.closeConnection();
+		}
+		
+		
 	}
 
 }
--- src/db/DB.java
+++ src/db/DB.java
@@ -4,62 +4,87 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
-	
-	
-	//Objeto de conexão do banco de dados do jdbc;
+
+	// Objeto de conexão do banco de dados do jdbc;
 	private static Connection conn = null;
-	
-	
-	//Método para conectar ao banco de dados.
+
+	// Método para conectar ao banco de dados.
 	public static Connection getConnection() {
-		//Caso ainda seja nulo
-		if(conn == null) {
-			try{
-			//Pegar as propriedades do BD
-			Properties props = loadingProperties();
-			//Pega a URL do Banco de dados
-			String url = props.getProperty("dburl");
-			//Para obter uma conexão com o banco de dados é necessário chamar:
-			conn = DriverManager.getConnection(url, props);
+		// Caso ainda seja nulo
+		if (conn == null) {
+			try {
+				// Pegar as propriedades do BD
+				Properties props = loadingProperties();
+				// Pega a URL do Banco de dados
+				String url = props.getProperty("dburl");
+				// Para obter uma conexão com o banco de dados é necessário chamar:
+				conn = DriverManager.getConnection(url, props);
 			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 		return conn;
 	}
-	
-	
-	//Método para fechar a conexão com o banco de dados
+
+	// Método para fechar a conexão com o banco de dados
 	public static void closeConnection() {
-		//Se a conexão estiver instanciada
-		if(conn != null) {
+		// Se a conexão estiver instanciada
+		if (conn != null) {
 			try {
-			conn.close();
-			} catch(SQLException e) {
+				conn.close();
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 	}
-	
+
 	// Método auxiliar para carregar as propriedades do arquivo db.properties;
-	
+
 	private static Properties loadingProperties() {
-			//Para ler o arquivo na url "db.properties";
-			try(FileInputStream fs = new FileInputStream("db.properties")) {
-				
-				Properties props = new Properties();
-				//.load faz a leitura do arquivo  apontado pelo FS e guarda os dados no objeto props
-				props.load(fs);
-				return props;
-			} catch(IOException e) {
-				//Se der um erro IOException
-				//Vai lançar a exceção personalizada
+		// Para ler o arquivo na url "db.properties";
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+
+			Properties props = new Properties();
+			// .load faz a leitura do arquivo apontado pelo FS e guarda os dados no objeto
+			// props
+			props.load(fs);
+			return props;
+		} catch (IOException e) {
+			// Se der um erro IOException
+			// Vai lançar a exceção personalizada
+			throw new DbException(e.getMessage());
+		}
+	}
+
+	// Método para fechar um objeto do tipo statement
+	public static void closeStatement(Statement st) {
+		// Se o ST for diferente de nulo(ou seja, se estiver instanciado, entra na
+		// condição
+
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 
+	}
+	
+	
+	public static void closeResultSet(ResultSet rs) {
+		if(rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
 }
